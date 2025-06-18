5f8856d984607a328d2caf44d4ebebb87fa61ccd
Milk-Cool <coder.tomsk@gmail.com>
2025-06-18 20:35:25 +0300

    strip ports node

--- frontend/src/nodes/index.ts
+++ frontend/src/nodes/index.ts
@@ -4,9 +4,11 @@ import { LogVulnerable } from "./log";
 import { FilterHasPort } from "./filterHasPort";
 import { IPRange } from "./ipRange";
 import { FilterNmap } from "./filterNmap";
+import { MapStripPorts } from "./mapStripPort";
 
 LiteGraph.registerNodeType("overflow/const", ConstList);
 LiteGraph.registerNodeType("overflow/log", LogVulnerable);
 LiteGraph.registerNodeType("overflow/hasport", FilterHasPort);
 LiteGraph.registerNodeType("overflow/iprange", IPRange);
 LiteGraph.registerNodeType("overflow/nmap", FilterNmap);
+LiteGraph.registerNodeType("overflow/stripports", MapStripPorts);
---
+++ frontend/src/nodes/mapStripPort.ts
@@ -0,0 +1,13 @@
+import { LGraphNode, LiteGraph } from "litegraph.js";
+
+export class MapStripPorts extends LGraphNode {
+    constructor() {
+        super();
+        this.addInput("Targets", LiteGraph.ACTION);
+        this.addOutput("Targets", LiteGraph.EVENT);
+    }
+    title = "Strip ports";
+    async onAction(action, data) {
+        this.triggerSlot(0, data.map(x => x.replace(/:\d+$/, "")));
+    }
+}
