d51a0ca7f119b1a6a3734e293ae7c7a85f1af724
Milk-Cool <coder.tomsk@gmail.com>
2025-06-19 08:36:28 +0300

    disable access from other machines

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ import ViteExpress from "vite-express";
 import apiRouter from "./api/index.js";
 
 const app = express();
+app.use((req, res, next) => {
+    if(req.ip !== "127.0.0.1" && req.ip !== "::1")
+        res.status(400).send("Access not allowed from other machines");
+    else next();
+});
+
 app.use("/api", apiRouter);
 
 ViteExpress.bind(app, app.listen(2003, "localhost", () => {
