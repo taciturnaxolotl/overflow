1e1ebe80a16866702d9daa56715b3a80684d1618
Milk-Cool <coder.tomsk@gmail.com>
2025-06-20 12:23:47 +0300

    try/catch in fetchErr

--- frontend/src/fetch.ts
+++ frontend/src/fetch.ts
@@ -2,12 +2,17 @@ import { logYellow } from "./log";
 import { Task } from "./tasks";
 
 export const fetchErr = async (url: string | URL, opts?: RequestInit, task?: Task): Promise<Response> => {
-    const f = await fetch(url, Object.assign({ redirect: "follow" }, opts));
-    if(!f.ok) {
-        const text = `Request to ${url} failed with status ${f.status} ${f.statusText}. You might want to check logs.\n${await f.text()}`;
+    const onErr = async (f: null | Response = null) => {
+        const text = f === null
+            ? `Request to ${url} failed`
+            : `Request to ${url} failed with status ${f.status} ${f.statusText}. You might want to check logs.\n${await f.text()}`;
         logYellow(text);
         task?.remove();
-        throw new Error(text);
+        return new Error(text);
     }
-    return f;
+    try {
+        const f = await fetch(url, Object.assign({ redirect: "follow" }, opts));
+        if(!f.ok) throw await onErr(f);
+        return f;
+    } catch(_) { throw await onErr(); }
 };
